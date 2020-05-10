f00d64e410ee0e172bb20ea1ac6a815489e5f7ee
George Rappel <george.concei@hotmail.com>
2020-05-09 22:30:31 -0300

    + Default error message for resources not found

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,5 +6,11 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    /**
+     * @return \Illuminate\Http\JsonResponse Default error message with 404 HTTP code.
+     */
+    function resource_error(){
+        //TODO Localization
+        return response()->json(['error' => 'Recurso inexistente'], 404);
+    }
 }
